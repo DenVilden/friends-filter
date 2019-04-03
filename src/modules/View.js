6ea5fceccda0d1a5d576a5f02217f629243416b3
Denis Tolmachev <oovilden@gmail.com>
2019-04-03 18:06:30 +0500

    publish

--- src/modules/View.js
+++
@@ -1,8 +0,0 @@
-import friendTemplate from '../templates/friend.hbs';
-
-export default class View {
-  static render(element, friends) {
-    const template = friendTemplate(friends);
-    element.innerHTML = template;
-  }
-}
